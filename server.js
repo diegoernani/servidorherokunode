6fa23ba50b98d41ab453805ded195193b9bf47ee
diegoernani <diegoernani@MacBook-Pro-de-Diego.local>
2020-01-18 14:19:33 -0300

    subindo HOME

--- server.js
+++ server.js
@@ -7,72 +7,71 @@ const PORT = process.env.PORT || 8877;
 // Then
 server.use(express.static(process.env.PWD + '/public'));
 
-server.get('/', (req, res) => {
-  res.send('Oi!');
-//   res.send(`<!DOCTYPE html>
-//   <html>
-//   <head>
-//   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-//   <style>
-//   .card {
-//     box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-//     max-width: 300px;
-//     margin: auto;
-//     text-align: center;
-//     font-family: arial;
-//   }
+server.get('/', (req, res) => { 
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <style>
+  .card {
+    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+    max-width: 300px;
+    margin: auto;
+    text-align: center;
+    font-family: arial;
+  }
   
-//   .title {
-//     color: grey;
-//     font-size: 18px;
-//   }
+  .title {
+    color: grey;
+    font-size: 18px;
+  }
   
-//   button {
-//     border: none;
-//     outline: 0;
-//     display: inline-block;
-//     padding: 8px;
-//     color: white;
-//     background-color: #000;
-//     text-align: center;
-//     cursor: pointer;
-//     width: 100%;
-//     font-size: 18px;
-//   }
+  button {
+    border: none;
+    outline: 0;
+    display: inline-block;
+    padding: 8px;
+    color: white;
+    background-color: #000;
+    text-align: center;
+    cursor: pointer;
+    width: 100%;
+    font-size: 18px;
+  }
   
-//   a {
-//     text-decoration: none;
-//     font-size: 22px;
-//     color: black;
-//   }
+  a {
+    text-decoration: none;
+    font-size: 22px;
+    color: black;
+  }
   
-//   button:hover, a:hover {
-//     opacity: 0.7;
-//   }
-//   </style>
-//   </head>
-//   <body>
+  button:hover, a:hover {
+    opacity: 0.7;
+  }
+  </style>
+  </head>
+  <body>
   
-//   <h2 style="text-align:center"></h2>
+  <h2 style="text-align:center"></h2>
   
-//   <div class="card">
-//     <img src="/team1.jpg" alt="Diego" style="width:100%">
-//     <h1>Diego Ernani</h1>
-//     <p class="title">Software Developer, Matriz</p>
-//     <p>Unopar</p>
-//     <div style="margin: 24px 0;">
-//       <a href="https://www.instagram.com/diegoernanisa/"><i class="fa fa-instagram"></i></a> 
-//       <a href="https://twitter.com/diegoernanisa"><i class="fa fa-twitter"></i></a>  
-//       <a href="https://www.linkedin.com/in/diegoernani"><i class="fa fa-linkedin"></i></a>  
-//       <a href="https://www.facebook.com/diegoernanisa"><i class="fa fa-facebook"></i></a> 
-//     </div>
-//     <p>
-//     <a href="https://diegoernani.pro"><button>Contato</button></a></p>
-//   </div>
+  <div class="card">
+    <img src="/team1.jpg" alt="Diego" style="width:100%">
+    <h1>Diego Ernani</h1>
+    <p class="title">Software Developer, Matriz</p>
+    <p>Unopar</p>
+    <div style="margin: 24px 0;">
+      <a href="https://www.instagram.com/diegoernanisa/"><i class="fa fa-instagram"></i></a> 
+      <a href="https://twitter.com/diegoernanisa"><i class="fa fa-twitter"></i></a>  
+      <a href="https://www.linkedin.com/in/diegoernani"><i class="fa fa-linkedin"></i></a>  
+      <a href="https://www.facebook.com/diegoernanisa"><i class="fa fa-facebook"></i></a> 
+    </div>
+    <p>
+    <a href="https://diegoernani.pro"><button>Contato</button></a></p>
+  </div>
   
-//   </body>
-//   </html>`
-// )
+  </body>
+  </html>`
+)
 });
 
 server.get('/contato', (req, res) => {
